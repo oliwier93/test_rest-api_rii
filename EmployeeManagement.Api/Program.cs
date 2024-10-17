90b4d13db8412de93fa819de4e1a9a0e80cd3b45
Oliwier Baran <oliwier.baran@icloud.com>
2024-10-17 14:50:56 +0200

    Package ver fixes

--- EmployeeManagement.Api/Program.cs
+++ EmployeeManagement.Api/Program.cs
@@ -1,13 +1,14 @@
 using EmployeeManagement.Domain.Services;
 using EmployeeManagement.Domain.Repositories;
 using EmployeeManagement.Domain.Handlers;
+using MediatR;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddSingleton<IRegistryNumberGenerator, RegistryNumberGenerator>();
 builder.Services.AddSingleton<IEmployeeRepository, InMemoryEmployeeRepository>();
 
-builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(CreateEmployeeHandler).Assembly));
+builder.Services.AddMediatR(typeof(CreateEmployeeHandler).Assembly);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
